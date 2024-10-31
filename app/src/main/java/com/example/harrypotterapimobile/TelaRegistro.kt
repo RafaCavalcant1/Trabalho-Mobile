2cc34079909a7255a8499324ad8841d31586073f
joaoferreiral <jvflsantos@gmail.com>
2024-10-31 19:53:48 -0300

    feat: Criação da tela de cadastro/registro

---
+++ app/src/main/java/com/example/harrypotterapimobile/TelaRegistro.kt
@@ -0,0 +1,11 @@
+package com.example.harrypotterapimobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TelaRegistro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_registro)
+    }
+}
